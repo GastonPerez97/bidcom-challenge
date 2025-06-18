b7e98f731abf922e560f12047a6a009dd13fbe47
Gastón Pérez <gastonperez97@gmail.com>
2025-06-17 23:40:36 -0300

    add bidcom api response types

---
+++ src/types/bidcom-api.ts
@@ -0,0 +1,10 @@
+export type BidcomInfo = {
+  headline: string;
+  title: string;
+  description: string;
+  button: { title: string; link: string };
+};
+
+export type BidcomImage = {
+  image: string;
+};
