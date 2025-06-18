86c7c9c3e3574d8f9ac6948b9ba5a719673f0a26
Gastón Pérez <gastonperez97@gmail.com>
2025-06-17 21:58:42 -0300

    simplify navbar layout and modify padding

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import Sidenav from "./Sidenav";
 export default function Navbar() {
   return (
     <header>
-      <nav className="fixed top-0 left-0 z-50 flex w-full justify-end border-b border-white/20 bg-white/60 px-12 py-4 shadow-sm backdrop-blur-md">
+      <nav className="fixed top-0 left-0 z-50 flex w-full justify-end bg-white/60 px-24 py-5 backdrop-blur-md">
         <Sidenav />
       </nav>
     </header>
