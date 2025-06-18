96d94ae983404c802b1b050fee7c50e5ebdae831
Gastón Pérez <gastonperez97@gmail.com>
2025-06-18 19:56:20 -0300

    add allowed image hosts in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "unsplash.com" },
+      { protocol: "https", hostname: "www.pexels.com" },
+      { protocol: "https", hostname: "pixabay.com" },
+      { protocol: "https", hostname: "www.publicdomainpictures.net" },
+      { protocol: "https", hostname: "www.freepik.com" }
+    ]
+  }
 };
 
 export default nextConfig;
