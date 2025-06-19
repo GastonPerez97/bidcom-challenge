a2a25c081de28cfdc1e5bf1a3302fea7fd3bb5a3
Gastón Pérez <gastonperez97@gmail.com>
2025-06-18 22:56:03 -0300

    add hero fallback content

--- src/consts/hero-fallback-images.ts
+++ src/consts/hero-fallback-images.ts
@@ -1,3 +1,16 @@
+import { BidcomInfo } from "@/types/bidcom-api";
+
+export const HERO_FALLBACK_CONTENT: BidcomInfo = {
+  headline: "About -- Personal",
+  title: "Hello! I'm <strong>Gastón Pérez</strong>",
+  description:
+    "Fallback lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  button: {
+    title: "Resume",
+    link: "https://gastonperez.ar/resume"
+  }
+};
+
 export const HERO_FALLBACK_IMAGES = [
   {
     src: "/images/hero/hero-fallback-1.webp",
