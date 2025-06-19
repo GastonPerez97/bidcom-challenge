b08b8d453a13917eda49afc9e250728b2ff13620
Gastón Pérez <gastonperez97@gmail.com>
2025-06-18 22:57:05 -0300

    handle errors in content service

--- src/services/content-service.ts
+++ src/services/content-service.ts
@@ -4,10 +4,27 @@ import { makeRequest } from "@/lib/fetch";
 import { BidcomImage, BidcomInfo } from "@/types/bidcom-api";
 
 export async function getContent() {
-  const [content, images] = await Promise.all([
-    makeRequest<BidcomInfo>(ENV.BIDCOM_API_BASE_URL + BIDCOM_API_ROUTES.CHALLENGE_INFO),
-    makeRequest<BidcomImage[]>(ENV.BIDCOM_API_BASE_URL + BIDCOM_API_ROUTES.CHALLENGE_IMAGES)
-  ]);
+  let content: BidcomInfo | null = null;
+  let images: BidcomImage[] | null = null;
 
-  return { content, images };
+  try {
+    [content, images] = await Promise.all([
+      makeRequest<BidcomInfo>(ENV.BIDCOM_API_BASE_URL + "BIDCOM_API_ROUTES.CHALLENGE_INFO").catch(
+        (error) => {
+          console.error("Failed to fetch Bidcom content:", error);
+          return null;
+        }
+      ),
+      makeRequest<BidcomImage[]>(
+        ENV.BIDCOM_API_BASE_URL + BIDCOM_API_ROUTES.CHALLENGE_IMAGES
+      ).catch((error) => {
+        console.error("Failed to fetch Bidcom images:", error);
+        return null;
+      })
+    ]);
+  } catch (error) {
+    console.error("Error fetching Bidcom content and images:", error);
+  } finally {
+    return { content, images };
+  }
 }
