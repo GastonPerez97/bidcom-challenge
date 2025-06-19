8f46d0473f4dfb6b29573bc52d9ad58215d009f7
Gastón Pérez <gastonperez97@gmail.com>
2025-06-18 23:13:41 -0300

    fix burger menu movement when opening sidebar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import Sidenav from "./Sidenav";
 export default function Navbar() {
   return (
     <header>
-      <nav className="fixed top-0 left-0 z-50 flex w-full justify-end bg-white/60 px-24 py-5 backdrop-blur-md">
+      <nav className="fixed top-0 left-0 z-50 flex w-dvw justify-end bg-white/60 px-24 py-5 backdrop-blur-md">
         <Sidenav />
       </nav>
     </header>
