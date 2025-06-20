00dd68f46433babed1f7d9eb1e8effbc2ea130c1
Gastón Pérez <gastonperez97@gmail.com>
2025-06-20 18:36:42 -0300

    use section header in contact and services sections

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,21 +1,20 @@
 import ContactForm from "@/components/contact/ContactForm";
 import ContactMoreInfo from "@/components/contact/ContactMoreInfo";
 
+import SectionHeader from "../ui/SectionHeader";
+
 export default function Contact() {
   return (
-    <section
-      id="contact"
-      className="bg-muted/50 mx-auto w-full px-4 py-12 md:px-6 md:py-24 lg:py-32"
-    >
-      <div className="flex flex-col items-center justify-center space-y-4 text-center">
-        <div className="space-y-4">
-          <h2 className="text-3xl font-bold sm:text-5xl">Contact</h2>
-          <p className="text-muted-foreground flex max-w-[900px] flex-col md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+    <section id="contact" className="mx-auto w-full px-4 py-12 md:px-6 md:py-24 lg:pt-32 lg:pb-12">
+      <SectionHeader
+        title="Contact"
+        subtitle={
+          <>
             <span>Have a question or want to work together? I&apos;d love to hear from you.</span>
             <span>Send me a message and i&apos;ll respond as soon as possible.</span>
-          </p>
-        </div>
-      </div>
+          </>
+        }
+      />
 
       <div className="mx-auto grid max-w-5xl items-start gap-8 py-12 lg:grid-cols-2 lg:gap-12">
         <ContactForm />
--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -5,6 +5,7 @@ import { BENEFITS, SERVICES } from "@/consts/service-data";
 import { Benefit } from "@/types/services/benefit";
 
 import CallToAction from "../ui/CallToAction";
+import SectionHeader from "../ui/SectionHeader";
 import ServiceBenefit from "./ServiceBenefit";
 import ServiceCard from "./ServiceCard";
 
@@ -12,21 +13,20 @@ export default function Services() {
   return (
     <section id="services" className="mx-auto w-full px-4 py-12 lg:pt-20 lg:pb-0">
       <div className="flex flex-col items-center justify-center space-y-4 text-center">
-        <div className="mb-16 flex flex-col items-center justify-center space-y-4 text-center">
-          <div className="space-y-2">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">What I Offer</h2>
-
-            <p className="text-muted-foreground flex max-w-[900px] flex-col md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+        <SectionHeader
+          title="What I Offer"
+          subtitle={
+            <>
               <span>Comprehensive solutions tailored to your unique needs.</span>
               <span>
                 From strategy to execution, I provide professional services that deliver measurable
                 results.
               </span>
-            </p>
-          </div>
-        </div>
+            </>
+          }
+        />
 
-        <div className="mx-auto max-w-7xl">
+        <div className="mx-auto mt-12 max-w-7xl">
           <div className="mb-16 grid gap-6 md:grid-cols-3">
             {SERVICES.map((service, index) => (
               <ServiceCard key={index} {...service} />
