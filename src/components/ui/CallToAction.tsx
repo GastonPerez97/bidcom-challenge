db90dc6edb3ed9d8ca8dd6d2baa193a1641cbb9a
Gastón Pérez <gastonperez97@gmail.com>
2025-06-20 22:58:49 -0300

    add optional classname prop in call to action component

--- src/components/ui/CallToAction.tsx
+++ src/components/ui/CallToAction.tsx
@@ -2,12 +2,17 @@ import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/Button";
+import { cn } from "@/lib/utils";
 
-type CallToActionProps = { href: string; text: string };
+type CallToActionProps = { href: string; text: string; className?: string };
 
-export default function CallToAction({ href, text }: CallToActionProps) {
+export default function CallToAction({ href, text, className }: CallToActionProps) {
   return (
-    <Button size="xl" className="group w-full transition-all sm:w-[300px] lg:w-auto" asChild>
+    <Button
+      size="xl"
+      className={cn("group w-full transition-all sm:w-[300px] lg:w-auto", className)}
+      asChild
+    >
       <Link href={href}>
         {text} <ArrowRight className="duration-150 group-hover:translate-x-1" />
       </Link>
